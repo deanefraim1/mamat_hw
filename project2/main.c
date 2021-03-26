5e426723d13dde6f7b43a08f0fec5afb970f340b
Dean Efraim <deanefraim@gmail.com>
2021-03-26 17:49:40 +0300

    Initial Commit

---
+++ project2/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  project2
+//
+//  Created by Dean Efraim on 26/03/2021.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
